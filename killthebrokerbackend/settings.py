c25dd63e71ee3104645679ed76c07de38212e309
Cyprian Munene <munenecyp@gmail.com>
2022-11-30 14:29:25 +0300

    make staticfiles reachable

--- killthebrokerbackend/settings.py
+++ killthebrokerbackend/settings.py
@@ -127,6 +127,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
